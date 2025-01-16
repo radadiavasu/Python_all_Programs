1b65996baa8b2f40dc7c4c7bc1338ba8438c65d6
Vasu Radadia <114792837+radadiavasu@users.noreply.github.com>
2025-01-16 18:06:17 +0530

    Star-Patterns

---
+++ all_star.py
@@ -0,0 +1,73 @@
+#     *
+#    ***
+#   *****
+#  *******
+# *********
+#  *******
+#   *****
+#    ***
+#     *
+
+# n = 5
+
+# for i in range(n):
+#     # print("*" * (n-i*1))
+#     print(" " * (n-i-1) + "*" * (2*i+1))
+# for i in range(n-2, -1, -1):
+#     print(" " * (n-i-1) + "*" * (2*i+1))
+
+
+
+# * 
+# * * 
+# * * * 
+# * * * * 
+# * * * * * 
+# * * * * 
+# * * * 
+# * * 
+# * 
+
+# n = 5
+
+# for i in range(n):
+#     for j in range(i+1):
+#         print("*", end=" ")
+#     print()
+    
+# for i in range(n, 0, -1):
+#     for j in range(2, i+1):
+#         print("*", end=" ")
+#     print()
+
+
+
+# * 
+# * * 
+# * * * 
+# * * * * 
+# * * * * *
+ 
+# n = 5
+
+# for i in range(n):
+#     for j in range(i+1):
+#         print("*", end=" ")
+#     print()
+
+
+
+# * * * * * 
+# * * * * 
+# * * * 
+# * * 
+# *
+
+# n = 5
+
+# for i in range(n, 0, -1):
+#     for j in range(i):
+#         print("*", end=" ")
+#     print()
+
+
